7b159971387281509837991953cbb7cc1be85d95
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-06-20 15:20:38 +0400

    Route object

---
+++ benzole/core/route.py
@@ -0,0 +1,16 @@
+class Route:
+    
+    def GET(self):
+        return None
+    
+    def POST(self):
+        return None
+    
+    def PUT(self):
+        return None
+    
+    def PATCH(self):
+        return None
+    
+    def DELETE(self):
+        return None
